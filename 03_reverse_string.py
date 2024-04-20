11b83fb581fa74710e95328b8997eef597d6b09c
unknown <diloch111@gmail.com>
2024-04-20 01:24:47 +0100

    reverse characters in a string

---
+++ 03_reverse_string.py
@@ -0,0 +1,6 @@
+# Exercise 03: 
+# Write a function to reverse the characters in a given string.
+
+def reverse_string(string):         # define function with string parameter
+    return string[::-1]             # return the reversed string
+print(reverse_string("oloche aboje"))
